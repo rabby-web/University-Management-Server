e76c5764bb9c6ef3fdb337ea6d44893f0b804c4b
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-18 10:31:33 +0600

    feat(student): add functionality to create a new student record in the database
    - Implemented createStudentIntoDB function in StudentServices to add a new student to the database.
    - Created createStudent controller to handle student creation requests, receiving data from the client and sending a success response.
    - Set up a new route (POST /create-student) in the router to call the student creation controller function.
    - Added error handling and structured JSON response to indicate success or failure.

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -88,4 +88,4 @@ const studentSchema = new Schema<Student>({
 });
 
 // create model
-const Student = model<Student>('Student', studentSchema);
+export const StudentModel = model<Student>('Student', studentSchema);
