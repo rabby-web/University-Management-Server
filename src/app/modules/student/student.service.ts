4c32ee53c05e4e89d201e79cfa9c46700580a268
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-20 10:14:31 +0600

    Refactor student creation to use instance method
    - Replace StudentModel.create() static method with instance creation and student.save() to save the student data to the database
    - Ensure compatibility with the current database model setup by creating an instance of StudentModel directly with studentData
    - Streamline the save process with student.save() for improved readability and method control

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,14 +1,18 @@
 import { Student } from './student.interface';
 import { StudentModel } from './student.model';
 
-const createStudentIntoDB = async (student: Student) => {
-  const result = await StudentModel.create(student);
+const createStudentIntoDB = async (studentData: Student) => {
+  // const result = await StudentModel.create(student); // built in static method
+  const student = new StudentModel(studentData);
+  const result = await student.save(); // built in instance method
   return result;
 };
+
 const getAllStudentsFromDB = async () => {
   const result = await StudentModel.find();
   return result;
 };
+
 const getSingleStudentFromDB = async (id: string) => {
   const result = await StudentModel.findOne({ id });
   return result;
