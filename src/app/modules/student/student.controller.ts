075d93d5d2868fdab750b904d730c35ea4586b84
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-30 09:21:13 +0600

    Refactor: Add async error handling utility for controllers
    - Implemented catchAsync utility function to handle async errors in Express controllers.
    - Simplifies controller functions by centralizing error handling logic.
    - Ensures unhandled errors are passed to Express's default error handler using
    ext.
    - Improves maintainability and reduces boilerplate code in controllers.

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,15 +1,9 @@
-import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { StudentServices } from './student.service';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 
-const catchAsync = (fn: RequestHandler) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
-  };
-};
-
-const getAllStudents = catchAsync(async (req, res, next) => {
+const getAllStudents = catchAsync(async (req, res) => {
   const result = await StudentServices.getAllStudentsFromDB();
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -19,7 +13,7 @@ const getAllStudents = catchAsync(async (req, res, next) => {
   });
 });
 
-const getSingleStudents = catchAsync(async (req, res, next) => {
+const getSingleStudents = catchAsync(async (req, res) => {
   const { studentId } = req.params;
   const result = await StudentServices.getSingleStudentFromDB(studentId);
 
@@ -31,7 +25,7 @@ const getSingleStudents = catchAsync(async (req, res, next) => {
   });
 });
 
-const deleteStudent: RequestHandler = catchAsync(async (req, res, next) => {
+const deleteStudent = catchAsync(async (req, res) => {
   const { studentId } = req.params;
   const result = await StudentServices.deleteStudentFromDB(studentId);
 
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,40 +1,23 @@
-import {  RequestHandler } from 'express';
 import { UserService } from './user.service';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 
+const createStudent = catchAsync(async (req, res) => {
+  const { password, student: studentData } = req.body;
 
+  // data validation with zod
+  // const zodParseData = studentValidationSchema.parse(studentData);
 
+  const result = await UserService.createStudentIntoDB(password, studentData);
 
-
-const createStudent:RequestHandler = async (
-  req,
-  res,
-  next,
-) => {
-  try {
-    const { password, student: studentData } = req.body;
-
-    // data validation with zod
-    // const zodParseData = studentValidationSchema.parse(studentData);
-
-    const result = await UserService.createStudentIntoDB(password, studentData);
-
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student Created successfully',
-    //   data: result,
-    // });
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student Created successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student Created successfully',
+    data: result,
+  });
+});
 
 export const UserController = {
   createStudent,
