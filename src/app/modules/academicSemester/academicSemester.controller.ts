acc9110a52ad07325bfc184fe04276663341e304
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-01 14:38:29 +0600

    feat: integrate academic semester creation with database
    - Added createAcademicSemesterIntoDB service to handle database operations.
    - Used the AcademicSemester model to store academic semester data.
    - Updated the POST route to include request validation with AcademicSemesterValidations.createAcademicSemesterValidationSchema.
    - Ensured validation occurs before calling the createAcademicSemester controller.

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,11 +1,12 @@
 import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
+import { AcademicSemesterServices } from './academicSemester.service';
 
 const createAcademicSemester = catchAsync(async (req, res) => {
-  const result = req.body;
-
-  // const result = await UserService.createStudentIntoDB(password, studentData);
+  const result = await AcademicSemesterServices.createAcademicSemesterIntoDB(
+    req.body,
+  );
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
