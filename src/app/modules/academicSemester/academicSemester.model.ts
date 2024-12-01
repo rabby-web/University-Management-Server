2bdc4f3d479fa3cd643142ad71d52accdeaee475
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-01 12:05:27 +0600

    feat: add validation schema for academic semester creation
    - Implemented createAcademicSemesterValidationSchema using Zod for structured validation
    - Included validation for
    ame, year, code, startMonth, and endMonth fields
    - Used enums for AcademicSemesterName, AcademicSemesterCode, and Months to ensure valid input values
    - Exported AcademicSemesterValidations for reuse in the application

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -38,17 +38,17 @@ const academicSemesterSchema = new Schema<TAcademicSemester>(
   },
 );
 
-academicSemesterSchema.pre('save', async function (next) {
-  const isSemesterExists = await AcademicSemester.findOne({
-    year: this.year,
-    name: this.name,
-  });
+// academicSemesterSchema.pre('save', async function (next) {
+//   const isSemesterExists = await AcademicSemester.findOne({
+//     year: this.year,
+//     name: this.name,
+//   });
 
-  if (isSemesterExists) {
-    throw new Error('Semester is already exists !');
-  }
-  next();
-});
+//   if (isSemesterExists) {
+//     throw new Error('Semester is already exists !');
+//   }
+//   next();
+// });
 
 export const AcademicSemester = model<TAcademicSemester>(
   'AcademicSemester',
