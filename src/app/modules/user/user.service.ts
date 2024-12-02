fb112b806176aa1979db34c7099c67eb9cf564e1
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-02 10:23:34 +0600

    feat: implement student ID generation and role assignment
    - Added indLastStudentId utility to fetch the last created student ID for incremental ID generation.
    - Developed generateStudentId function to create unique student IDs based on academic semester year, code, and sequential numbering.
    - Set the default role as student during user creation.
    - Integrated academic semester lookup to use semester details for ID generation.
    - Implemented user creation logic with dynamically generated student IDs.

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,37 +1,41 @@
 import config from '../../config';
-// import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TStudent } from '../student/student.interface';
 import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
+import { generateStudentId } from './user.utils';
 
-const createStudentIntoDB = async (password: string, studentData: TStudent) => {
+const createStudentIntoDB = async (password: string, payload: TStudent) => {
   // create a user object
   const userData: Partial<TUser> = {};
-  // if password is not given, use default password
+
+  //if password is not given , use deafult password
   userData.password = password || (config.default_password as string);
 
-  // set student role
+  //set student role
   userData.role = 'student';
 
-  // year semesterCode 4 digit number
-  // const generateStudentId= (payload: TAcademicSemester)=>{
+  // find academic semester info
+  const admissionSemester = await AcademicSemester.findById(
+    payload.admissionSemester,
+  );
 
-  // }
+  //set  generated id
+  userData.id = await generateStudentId(admissionSemester);
 
-  //   // set manually generated it
-  //   userData.id = generateStudentId();
   // create a user
   const newUser = await User.create(userData);
-  // create a student
+
+  //create a student
   if (Object.keys(newUser).length) {
     // set id , _id as user
-    studentData.id = newUser.id;
-    studentData.user = newUser._id; //reference
-    const newStudent = await Student.create(studentData);
+    payload.id = newUser.id;
+    payload.user = newUser._id; //reference _id
+
+    const newStudent = await Student.create(payload);
     return newStudent;
   }
-  return newUser;
 };
 
 export const UserService = {
