58e383f50411a62698c94f513d1d2a50481748cd
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-03 18:22:52 +0600

    feat: implement custom error class for centralized error handling
    - Created AppError class extending the native Error class.
    - Added statusCode property to handle HTTP status codes effectively.
    - Implemented stack trace capture for better debugging.
    - Included optional custom stack trace initialization.

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,5 +1,7 @@
 import { Schema, model } from 'mongoose';
+import httpStatus from 'http-status';
 import { TAcademicDepartment } from './academicDepartment.interface';
+import AppError from '../../errors/AppError';
 const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   {
     name: {
@@ -23,7 +25,10 @@ academicDepartmentSchema.pre('save', async function (next) {
   });
 
   if (isDepartmentExist) {
-    throw new Error('This department is already exist!');
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'This department is already exist!',
+    );
   }
 
   next();
@@ -34,7 +39,10 @@ academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
   const isDepartmentExist = await AcademicDepartment.findOne(query);
 
   if (!isDepartmentExist) {
-    throw new Error('This department does not exist! ');
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'This department does not exist! ',
+    );
   }
 
   next();
