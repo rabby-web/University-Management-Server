32a1f7e48702e913ecb6d874f6fc04f0acf1c85b
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-15 12:01:42 +0600

    feat: add refresh token validation and controller for token renewal
    - Created efreshTokenValidationSchema using Zod to validate refresh token from cookies.
    - Implemented efreshToken controller to handle token renewal:
      - Validates the provided refresh token.
      - Retrieves a new access token using AuthServices.refreshToken.
      - Sends a successful response with the new access token.
    - Added /refresh-token route with validation middleware for secure token renewal.

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import cors from 'cors';
 import globalErrorHandler from './app/middleware/globalErrorHandler';
 import notFound from './app/middleware/notFound';
 import router from './app/routes';
+import cookieParser from 'cookie-parser';
 const app: Application = express();
 
 // parsers
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({ origin: ['http://localhost:5000'] }));
 
 // application routes
 app.use('/api/v1', router);
--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -14,10 +14,15 @@ router.post(
 );
 router.post(
   '/change-password',
-  auth(USER_ROLE.admin,USER_ROLE.faculty,USER_ROLE.student),
+  auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
   validateRequest(AuthValidation.changePasswordValidationSchema),
   AuthControllers.changePassword,
 );
 
+router.post(
+  '/refresh-token',
+  validateRequest(AuthValidation.refreshTokenValidationSchema),
+  AuthControllers.refreshToken,
+);
 
 export const AuthRoutes = router;
