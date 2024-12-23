122b58d56c3d54199669eaeeab272eeecd889c99
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-23 10:00:21 +0600

    Refactor email utility: improve modularity and security
    - Moved SMTP credentials to the configuration file for better security.
    - Added error handling in the sendEmail function.
    - Created a separate function for sending password reset emails.
    - Improved HTML email template for a better user experience.

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -188,10 +188,10 @@ const forgetPassword = async (userId: string) => {
     '10m',
   );
 
-  const resetUILink = `http://localhost:3000/?id=${user.id}&token=${resetToken}`;
+  const resetUILink = `${config.reset_pass_ui_link}?id=${user.id}&token=${resetToken}`;
   console.log(resetUILink);
 
-  sendEmail();
+  sendEmail(user.email, resetUILink);
 };
 
 export const AuthService = {
--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -1,21 +1,22 @@
 import nodemailer from 'nodemailer';
+import config from '../config';
 
-export const sendEmail = async () => {
+export const sendEmail = async (to: string, html: string) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
+    host: 'smtp.gmail.com',
     port: 587,
-    secure: false, // true for port 465, false for other ports
+    secure: config.NODE_ENV === 'production', // true for port 465, false for other ports
     auth: {
-      user: 'maddison53@ethereal.email',
-      pass: 'jn7jnAPss4f63QBp6D',
+      user: 'rabby.webdeveloper@gmail.com',
+      pass: 'rjsu rsru lijr sqxx',
     },
   });
 
   await transporter.sendMail({
-    from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
-    to: 'bar@example.com, baz@example.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world?', // plain text body
-    html: '<b>Hello world?</b>', // html body
+    from: 'rabby.webdeveloper@gmail.com', // sender address
+    to, // list of receivers
+    subject: 'Please change your password ✔', // Subject line
+    text: 'Reset your password within 10 minutes!', // plain text body
+    html, // html body
   });
 };
