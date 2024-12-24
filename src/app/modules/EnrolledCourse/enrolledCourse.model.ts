2cbd3f072052fb78b1c45549ec121bfd3cad97f2
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-24 09:57:29 +0600

    feat: add Mongoose schema for course marks with validation
    - Implemented courseMarksSchema to define structure and validation for course marks.
    - Included fields: classTest1, classTest2, midTerm, and inalTerm with min, max, and default values.
    - Set _id: false to use the schema as a subdocument without an additional ID.

--- src/app/modules/EnrolledCourse/enrolledCourse.model.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.model.ts
@@ -1,2 +1,34 @@
+import { Schema } from 'mongoose';
+import { TEnrolledCourseMarks } from './enrolledCourse.interface';
 
-
+const courseMarksSchema = new Schema<TEnrolledCourseMarks>(
+  {
+    classTest1: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    midTerm: {
+      type: Number,
+      min: 0,
+      max: 30,
+      default: 0,
+    },
+    classTest2: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    finalTerm: {
+      type: Number,
+      min: 0,
+      max: 50,
+      default: 0,
+    },
+  },
+  {
+    _id: false,
+  },
+);
