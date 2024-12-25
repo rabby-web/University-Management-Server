6df1070f1143dea42d840fa45e8ecc924ad4c90a
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-25 11:33:34 +0600

    feat: Add validations for student existence and duplicate enrollment
    - Added check to verify if the student exists in the database using Student.findOne.
    - Implemented logic to prevent duplicate enrollments by verifying existing records in EnrolledCourse.
    - Enhanced error handling with meaningful error messages for missing students and duplicate enrollment conflicts.

--- src/app/modules/EnrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.service.ts
@@ -2,6 +2,8 @@ import AppError from '../../errors/AppError';
 import { OfferedCourse } from '../OfferedCourse/OfferedCourse.model';
 import { TEnrolledCourse } from './enrolledCourse.interface';
 import httpStatus from 'http-status';
+import EnrolledCourse from './enrolledCourse.model';
+import { Student } from '../student/student.model';
 
 const createEnrolledCourseIntoDB = async (
   userId: string,
@@ -16,9 +18,29 @@ const createEnrolledCourseIntoDB = async (
 
   const { offeredCourse } = payload;
   const isOfferedCourseExists = await OfferedCourse.findById(offeredCourse);
+
   if (!isOfferedCourseExists) {
     throw new AppError(httpStatus.NOT_FOUND, 'Offered Courses not found');
   }
+
+  const student = await Student.findOne({
+    id: userId,
+  }).select('id');
+
+  if (!student) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Student not found');
+  }
+
+  const isStudentAlreadyEnrolled = await EnrolledCourse.findOne({
+    semesterRegistration: isOfferedCourseExists?.semesterRegistration,
+    offeredCourse,
+    student: student?.id,
+  });
+  if (isStudentAlreadyEnrolled) {
+    throw new AppError(httpStatus.CONFLICT, 'Student is already enrolled!');
+  }
+
+  
 };
 
 export const EnrolledCourseServices = {
