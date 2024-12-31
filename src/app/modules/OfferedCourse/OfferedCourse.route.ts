e5ac6e817504d30fd99e695d0fe997588485abbf
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-31 18:44:21 +0600

    use auth gurad in offerd course

--- src/app/modules/OfferedCourse/OfferedCourse.route.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.route.ts
@@ -2,20 +2,46 @@ import express from 'express';
 import { OfferedCourseValidations } from './OfferedCourse.validation';
 import validateRequest from '../../middleware/validateRequest';
 import { OfferedCourseControllers } from './OfferedCourse.controller';
+import auth from '../../middleware/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/create-offered-course',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
   validateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
   OfferedCourseControllers.createOfferedCourse,
 );
-router.get('/:id', OfferedCourseControllers.getSingleOfferedCourses);
-router.delete('/:id', OfferedCourseControllers.deleteOfferedCourseFromDB);
+router.get(
+  '/:id',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  OfferedCourseControllers.getSingleOfferedCourses,
+);
+router.delete(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  OfferedCourseControllers.deleteOfferedCourseFromDB,
+);
 
-router.get('/', OfferedCourseControllers.getAllOfferedCourses);
+router.get(
+  '/',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  OfferedCourseControllers.getAllOfferedCourses,
+);
 router.patch(
   '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
   OfferedCourseControllers.updateOfferedCourse,
 );
